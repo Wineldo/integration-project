5f8e8709db36f2ffc223c36a4f5d24c16d53e4b4
Wineldo Jean <73646831+Wineldo@users.noreply.github.com>
2020-12-01 22:02:17 -0500

    Update Main.py
    Enhance code quality and security.

--- Main.py
+++ Main.py
@@ -1,88 +1,118 @@
-# Wineldo Jean - 29 October 2020 (last modified)
-# A guessing game that allows the user to speficy a range and predict the number the program picked.
-# The user will have additional help such as 'near' or 'far' to approximate the program's number.
-# The program will limit the number of guesses based on the user's range.
-# No source(s) available at this moment
+"""A guessing game that allows the user to specify a range and predict the
+number the program picked. The user will have additional help such as 'near'
+or 'far' to approximate the program's number. The program will limit the
+number of guesses based on the user's range."""
+__author__ = "Wineldo Jean"
 
-import math, random
 
-# A list of names for the program to choose
-names = ['Jon', 'Bill', 'Maria', 'Jenny', 'Jack', 'Michael']
+import math  # Necessary to perform complex calculation
+import random  # Necessary for the 'Guess My Number' game
 
-python = names[random.randint(0,len(names)-1)]
-print('Welcome, I\'m ',python, '! \nI will be guiding you throughout the process.',sep='')
 
-user = input('\nMhmmm. You look awfully familiar.\nI think your name is: ').capitalize()
-print('\nYes, that\'s right! You\'re ',user,'! I have heard a lot about you.\nWait, let me make sure I got the right person...',sep='')
+def learn(user):
+    """
+    Summary line:
+    The integration project for Sprint One. It goes through all the
+    Python basic calculations, string operators, shortcut operators,
+    numeric operators, and simple I/O
 
-# The function learn() is the project for Sprint One
-def learn():
-    # Python mathematical symbol intrustions
-    print('\nOkay, ',user,', let me teach you the basics since you are new to Python.',sep='')
-    print('We are going to start with Python mathematical symbols\n')
+    Parameters:
+    user (str): the name of the user
+
+    Returns:
+    boolean: True after completing the program successfully
+    """
+
+    # Python mathematical symbol instructions
+    print("\nOkay,", user)
+    print("Let me teach you the basics since you are new.")
+    print("We are going to start with Python mathematical symbols\n")
 
     # Numeric operator: ** represents the power rule (2^5)
-    print('This ** represents the power rule in Python: 2**5 =',2**5)
+    print("This ** represents the power rule in Python: 2**5 =", 2 ** 5)
 
-    # Numeric operator: *  represents mulitpilcation (2*5)
-    print('This * represents mulitpilcation in Python: 2*5 =',2*5)
+    # Numeric operator: *  represents multiplication (2*5)
+    print("This * represents multiplication in Python: 2*5 =", 2 * 5)
 
     # Numeric operator: /  represents division       (2/5)
-    print('This / represents division in Python: 2/5 =',2/5)
+    print("This / represents division in Python: 2/5 =", 2 / 5)
 
     # Numeric operator: %  represents modulo         (2%5)
-    print('This %', 'represents the modulo (division remainder) in Python: 2%5 =',2%5,)
+    print("This %", "represents the modulo (division remainder) in Python:"
+                    " 2%5 =", 2 % 5, )
 
     # Numeric operator: // represents division       (2//5)
-    print('This // represents division (with no remainder) in Python: 2//5 =',2//5)
+    print("This // represents division (with no remainder) in Python:"
+          " 2//5 =", 2 // 5)
 
     # Numeric operator: +  represents addition       (2+5)
-    print('This + represents addition in Python: 2+5 =',2+5)
+    print("This + represents addition in Python: 2+5 =", 2 + 5)
 
     # Numeric operator: -  represents subtraction    (2-5)
-    print('This - represents subtraction in Python: 2-5 =',2-5)
+    print("This - represents subtraction in Python: 2-5 =", 2 - 5)
 
-    print('\nWe can use these mathematical symbols in a text or a string as a programmer would say.\n')
+    print("\nWe can use these mathematical symbols in a text or a string.\n")
 
     # String operator: * represents how times the string is repeated
-    print('This * is used to indicate how many times a text is repeated in Python.')
-    word   = str(input('For instance, please enter a word or a sentence: '))
-    repeat = int(input('How many times you wanted it to be repeated: '))
-    print('\n',word * repeat,'\n',sep='')   
+    print("This * is used to indicate how many times a text is repeated.")
+    word = str(input("For instance, please enter a word or a sentence: "))
+    while True:
+        try:
+            repeat = int(input("How many times you wanted it repeated: "))
+            break
+        except ValueError:
+            print("Error. Must be a whole number.")
 
-    # String operator: + represents concatenation or combination 
-    print('This + is used to combine two or more words in Python.')
-    word  = str(input('For example, please enter a word or a sentence: '))
-    word2 = str(input('Please enter another word or a sentence: '))
-    print('Space is not added. This the result of combining a string:',word+word2,'\n')
+    print("\n", word * repeat, "\n", sep='')
 
-    print('Wow, that is a lot to learn but we are not done yet!')
-    print('I am going to show you how to make a variable in Python.\n')
+    # String operator: + represents concatenation or combination
+    print("This + is used to combine two or more words in Python.")
+    word = str(input("For example, please enter a word or a sentence: "))
+    word2 = str(input("Please enter another word or a sentence: "))
+    print("This is the result of combining two strings:", word + word2, "\n")
 
-    # Assignment operator: = assign values to a variable 
-    print('This = symbol is completely different in Python.')
-    print('This is use to attach a value to a variable user')
-    var   = str(input('As a demostration, please enter any user to be use as a variable: '))
-    value = input('Think of anything you want the variable to be assign: ')
-    print('Your variable',var,'=',value,'\n')
+    print("Wow, that is a lot to learn but we are not done yet!")
+    print("I am going to show you how to make a variable in Python.\n")
 
-    print('Now, I think we are almost finish, is to give you useful tips and advice\n')
+    # Assignment operator: = assign values to a variable
+    print("This = symbol is completely different in Python.")
+    print("This is use to attach a value to any particular variable")
 
-    # Shortcut operators: += represents the incrementation 
-    print('This += increases any variable to its next level or number: 5 +=1 = 6')
-    # Shortcut operators: -= represents the decrementation 
-    print('This -= decreases any variable to its lower level or number: 5 -=1 = 4')
+    var = str(input("\nAs a demonstration, please type in a variable name: "))
+    value = input("Think of anything you want the variable to be assign: ")
 
-    print('\nYou have now learned all the basics\nBye,',user,'\n')
+    print("Your variable", var, "=", value, "\n")
+
+    print("Now, I think we are almost finish, is to give you useful tips\n")
+
+    # Shortcut operators: += represents the incrementation
+    print("This += increases any variable to its next number: 5 +=1 = 6")
+    # Shortcut operators: -= represents the decremental
+    print("This -= decreases any variable to its last number: 5 -=1 = 4")
+
+    print("\nYou have now learned all the basics.")
+    print("I hope you have found this useful.")
 
     return True
 
-# The function answer() takes the user's response and return True if user agrees with the question
+
 def answer(query):
-    allow = ['yes','yea', 'y', 'yeah']
-    never = ['no', 'nope', 'nah', 'n']
-       
-    select = input(query).strip()
+    """
+    Summary line:
+    It prompts and takes the user's response and return True if user
+    agrees with the question otherwise False
+
+    Parameters:
+    query (str): a question statement
+
+    Returns:
+    boolean: true or false based on user's response
+    function: recall the function if user's response is undetermined
+    """
+    allow = ["1", "yes", "yeah", "yea", "y"]
+    never = ["0", "no", "nope", "nah", "n"]
+
+    select = input(query).strip()  # Receive the user's response
 
     for item in range(len(allow)):
         if allow[item] == select.lower():
@@ -92,22 +122,39 @@ def answer(query):
         if never[item] == select.lower():
             return False
 
-    print('\nI\'m sorry but I could not understand your response.\nPlease try again.')
-    return answer(query)
+    print("\nI'm sorry but I could not understand your response.")
+    print("Please try again.")
+    return answer(query)  # Call the function again on unclear response
 
-# The function rules() showcases what is allow and not allow from playing the 'Guess My Number' game
-def rules():
-    print('\nThe rule has not been set in place yet.')
 
-# The function game() is the 'Guess My Number' game with complex logic
-def game(setup):
+def game(setup, players):
+    """
+    Summary line:
+    The core of the 'Guess My Number' game with complex logic
+
+    Parameters:
+    setup (arr): the procedure of the 'Guess My Number' game
+    players (arr): the names of the people who are playing
+
+    Returns:
+    str: congratulating the winner and exiting the game
+    function: play the game again with a new procedure
+    """
+    python = players[0]
+    user = players[1]
     tries = math.ceil(abs(setup[1] - setup[2]) * 0.20)
     num = setup[0]
     won = python
 
     while tries > 0:
-        print('\nThis is how many times you can guess:',tries)
-        guess = int(input('Predict my number: '))
+        print('\nThis is how many times you can guess:', tries)
+
+        while True:
+            try:
+                guess = int(input('Predict my number: '))
+                break
+            except ValueError:
+                print("Error. Must be a whole number.")
 
         approx = abs(guess - num)
         scope = math.ceil(abs(setup[1] - setup[2]) * 0.20)
@@ -119,30 +166,48 @@ def game(setup):
             print('\nYour guess is too far from my number.')
             print('Try getting closer.')
             tries -= 1
-        elif approx <= scope and approx > 0:
+        elif scope >= approx > 0:
             print('\nYour guess is near my number.')
             print('Proceed with caution!')
             tries -= 1
         else:
-            print('\nCongratulations, ',user,'!',sep='')
+            print('\nCongratulations, ', user, '!', sep='')
             print('You\'ve guessed my number!')
             tries = 0
             won = user
 
     if won == python:
-        print('\nYou have no more tries. My number was',num)
-        print('Hehe, I guess I won this round.')
+        print('\nYou have no more tries. My number was', num)
+        print('Hee, I guess I won this round.')
 
-    if not answer('\nWould you like to play again?\n'):
+    if not answer('\nWould you like to play again (Y/N)?\n'):
         return print('\nThanks for playing!')
-    
-    return game(random_number())
 
-# The function random_number() is responsible for returning a number within the range specify randomly 
+    return game(random_number(), players)
+
+
 def random_number():
+    """
+    Summary line:
+    This is responsible for returning a random number within the range
+    specify by the user
+
+    Parameters:
+    none
+
+    Returns:
+    arr: a list of the guess number, and the range such as initial and final
+    """
+
     print('\nPlease pick the range for the \'Guess My Number\' game\n')
-    initial = int(input('From: '))
-    final = int(input('To: '))
+
+    while True:
+        try:
+            initial = int(input('From: '))
+            final = int(input('To: '))
+            break
+        except ValueError:
+            print("Error. Must be a whole number.")
 
     if final < initial:
         print('\nRange complexion error.')
@@ -153,12 +218,55 @@ def random_number():
         print('\nRange complexion error.')
         print('The numbers are the same with no difference.\n')
         return random_number()
-    
-    return [random.randint(initial,final), final, initial]
 
-if answer('\nAre you famailiar with Python?\n'): 
-    print('\nAwesome, Let\'s get started!')
-    print('The winner takes it all.')
-    game(random_number())
-else:
-    learn()
+    return [random.randint(initial, final), final, initial]
+
+
+def welcome():
+    """
+    Summary line:
+    New member is greeted with an introduction message and
+    will register his/her name to system
+
+    Parameters:
+    none
+
+    Returns:
+    arr: a list of python's name and user chosen name.
+    """
+
+    # A list of names for the program to choose
+    names = ["Jon", "Bill", "Maria", "Jenny", "Jack", "Michael"]
+
+    python = names[random.randint(0, len(names) - 1)]
+    print("Welcome, I'm ", python, "!", sep='')
+    print("I will be guiding you throughout the process.")
+
+    print("\nHmm. You look awfully familiar.")
+    user = input("I think your name is: ").capitalize()
+
+    print("\nThat's right, You're ", user, "!", sep='')
+    print("I've heard a lot about you!")
+
+    return [python, user]
+
+
+def main():
+    """
+    Summary line:
+    Initialize the following program.
+
+    Parameters:
+    none
+
+    Returns:
+    function: initiate the game()
+    """
+    participant = welcome()  # Return a list of the names
+    if answer("\nDo you know Python (Y/N)?\n") or learn(participant[1]):
+        print("\nAll right, now that you've made it here")
+        print("Let's get started with the game!")
+        return game(random_number(), participant)
+
+
+main()
